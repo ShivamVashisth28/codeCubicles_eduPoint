a19ab2b18a5ff1177d9440ca76daa5dcbe8bdf7c
Shivam Vashisth <vashisth11shivam@gmail.com>
2024-09-15 13:33:46 +0530

    make backend changes for deploy

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json({ limit:"16kb"}))
 app.use(cors({
-    origin: process.env.CORS_ORIGIN|| "http://localhost:5173/"
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173/"
 }))
 
 app.get("/",(req,res) => {
